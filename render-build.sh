1734b9927a64de50ca17acfdae1ba54e61de698b
jasminarotella <164342084+jasminarotella@users.noreply.github.com>
2025-03-10 12:50:23 +0100

    uffa2

--- render-build.sh
+++ render-build.sh
@@ -2,18 +2,18 @@
 set -e  # Ferma lo script se un comando fallisce
 
 echo "🚀 Inizio build del frontend..."
-cd jasmina_rotella_frontend  # 🔹 CORRETTO IL PERCORSO
+cd /jasminarotella_frontend  # 🔹 CORRETTO IL PERCORSO
 npm install
 npm run build
 
 echo "✅ Creazione della cartella static nel backend..."
-mkdir -p ../jasmina_rotella_backend/app/static
+mkdir -p ../jasminarotella_backend/app/static
 
 echo "✅ Spostamento della build nel backend..."
-mv dist/* ../jasmina_rotella_backend/app/static/
+mv dist/* ../jasminarotella_backend/app/static/
 
 echo "🔧 Installazione delle dipendenze backend..."
-cd ../../../jasmina_rotella_backend
+cd ../../../jasminarotella_backend
 pip install -r requirements.txt
 
 echo "✅ Build completata con successo!"
