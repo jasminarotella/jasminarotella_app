a310a42fb3baebd53f6dc4278e71eac291c02fc7
jasminarotella <164342084+jasminarotella@users.noreply.github.com>
2025-03-10 12:25:26 +0100

    sistemata porta

--- jasminarotella_backend/app/run.py
+++ jasminarotella_backend/app/run.py
@@ -30,3 +30,5 @@ with app.app_context():
 
 if __name__ == "__main__":
     app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Usa la porta assegnata da Render
+app.run(host="0.0.0.0", port=port)
